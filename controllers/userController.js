0780814220b7b34cf9443f55f5787510be253f9d
Olosko23 <oloogeorge633@gmail.com>
2024-01-31 13:15:15 +0300

    'releases'

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,22 @@
-console.log("User Controller");
+import User from "../models/userModel.js";
+import asyncHandler from "express-async-handler";
+
+
+
+//Get all users
+const getAllUsers = asyncHandler(async (req, res) => {
+  try {
+    const users = await User.find();
+    if (!users) {
+      return res
+        .status(404)
+        .json({ message: "There are no users in the database" });
+    }
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
+
--- models/prModel.js
+++ models/prModel.js
@@ -3,7 +3,68 @@ import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
 //Pr might need to look for another name for press release
-const prSchema = Schema({});
+const prSchema = Schema(
+  {
+    author: {
+      type: String,
+      required: true,
+    },
+    language: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    targetCountry: {
+      type: String,
+    },
+    targetRegion: {
+      type: String,
+    },
+    embargoed: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    embargoedTill: {
+      type: Date,
+    },
+    factChecked: {
+      type: Boolean,
+      default: false,
+    },
+    reviewed: {
+      type: Boolean,
+      default: false,
+    },
+    reviewedBy: {
+      type: String,
+    },
+    audioFiles: [
+      {
+        title: String,
+        audioUrl: String,
+      },
+    ],
+    imageFIles: [
+      {
+        title: String,
+        imageUrl: String,
+      },
+    ],
+    videoFiles: [
+      {
+        title: String,
+        videoUrl: String,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Pr = model("Pr", prSchema);
 
